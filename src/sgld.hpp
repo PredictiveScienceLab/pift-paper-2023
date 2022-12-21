d28de4a9519c4c24d595ba44f65d0d9922b0ff1a
Ilias Bilionis <ebilionis@gmail.com>
2022-12-20 20:24:51 -0500

    Updated SGLD.

--- src/sgld.hpp
+++ src/sgld.hpp
@@ -1,15 +1,21 @@
-/* Implementation of Stochastic Gradient Langevin Dynamics.
- *
- * Author:
- *  Ilias Bilionis
- *
- * Date:
- *  12/16/2022
- *
- */
+// Implementation of Stochastic Gradient Langevin Dynamics.
+//
+// Reference:
+//  
+//  Welling & Teh, 2011, Bayesian Learning via stochastic gradient Langevin
+//  dynamics.
+//
+// Author:
+//  Ilias Bilionis
+//
+// Date:
+//  12/16/2022
+//
 
 #ifndef PIFT_OPT_HPP
 #define PIFT_OPT_HPP
+
+namespace pift {
 
 #include <random>
 #include <string>
@@ -18,7 +24,7 @@
 using namespace std;
 
 
-template <typename T>
+template<typename T>
 struct SGLDParams {
 
   SGLDParams() :
@@ -38,7 +44,7 @@ struct SGLDParams {
   T gamma;
   int init_it;
   bool save_to_file;
-  string out_file;
+  std::string out_file;
   int save_freq;
   bool disp;
   int disp_freq;
@@ -46,7 +52,7 @@ struct SGLDParams {
 
 // SGLD implementation.
 // It allocates its own memory.
-template <typename T, typename UE, typename R>
+template<typename T, typename UE, typename R>
 inline void sgld(UE& ue_func,
           T* w,
           const int& dim,
@@ -55,32 +61,32 @@ inline void sgld(UE& ue_func,
           const SGLDParams<T>& params = SGLDParams<T>()
 ) {
   T grad_w_H[dim];
-  normal_distribution<T> norm(0, 1);
+  std::normal_distribution<T> norm(0, 1);
   sgld(ue_func, w, dim, rng, num_samples, grad_w_H, norm, params);
 }
 
 // SGLD implementation that requires providing 
 // memory space grad_w.
-template <typename T, typename UE, typename R, typename D>
+template <typename T, typename UE, typename R, typename Dist>
 void sgld(UE& ue_func,
           T* w,
           const int& dim,
           R& rng,
           const int num_samples,
           T* grad_w_H,
-          D& norm,
+          Dist& norm,
           const SGLDParams<T>& params = SGLDParams<T>()
 ) {
-  //normal_distribution<T> norm(0, 1);
-  ofstream of;
 #ifndef DISABLE_SGLD_SAVE
+  std::ofstream of;
   if (params.save_to_file)
     of.open(params.out_file);
 #endif
   const int max_it = num_samples + params.init_it;
   for(int it=params.init_it; it<max_it; it++) {
-    const T epsilon = params.alpha / pow((params.beta + it + 1), params.gamma);
-    const T sqrt_2_epsilon = sqrt(2.0 * epsilon);
+    const T epsilon = params.alpha 
+      / std::pow((params.beta + it + 1), params.gamma);
+    const T sqrt_2_epsilon = std::sqrt(2.0 * epsilon);
     const T h_val = ue_func(w, grad_w_H);
     for(int i=0; i<dim; i++)
       w[i] += (-epsilon * grad_w_H[i] + sqrt_2_epsilon * norm(rng));
@@ -92,12 +98,13 @@ void sgld(UE& ue_func,
 #endif
 #ifndef DISABLE_SGLD_DISP
     if (params.disp && it % params.disp_freq == 0)
-      cout_vec(w, dim, to_string(it) + ": " + to_string(h_val) + " ");
+      cout_vec(w, dim, std::to_string(it) + ": " + std::to_string(h_val) + " ");
   }
 #endif
 #ifndef DISABLE_SGLD_SAVE
   if (params.save_to_file)
     of.close();
 #endif
-}
+} // sgld
+} // namespace pift
 #endif // PIFT_OPT_HPP
